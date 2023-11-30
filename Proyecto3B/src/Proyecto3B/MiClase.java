9fe60d15dbe993455e405b80766c4fb4206a3cb6
Sanyo2704 <rromero8703@ceuandalucia.es>
2023-11-30 16:38:58 +0100

    cambioB1

---
+++ Proyecto3B/src/Proyecto3B/MiClase.java
@@ -0,0 +1,10 @@
+package Proyecto3B;
+
+public class MiClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Álvaro Romero Fuentes");
+	}
+
+}
