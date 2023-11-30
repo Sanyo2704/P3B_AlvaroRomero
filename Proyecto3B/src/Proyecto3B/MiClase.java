043478ba9b9c514112dc0b9a3580e40a329a3c5b
Sanyo2704 <151049455+Sanyo2704@users.noreply.github.com>
2023-11-30 16:53:13 +0100

    Update MiClase.java

--- Proyecto3B/src/Proyecto3B/MiClase.java
+++ Proyecto3B/src/Proyecto3B/MiClase.java
@@ -5,6 +5,7 @@ public class MiClase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Álvaro Romero Fuentes");
+		System.out.println("Estoy llegando al final");
 	}
 
 }
